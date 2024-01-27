23475266f673f83fe1c151f902f3ee5d09ed0b95
Chaa Loftin <chaamail@yahoo.com>
2024-01-27 18:21:47 -0500

    added path parameters example

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "my user id"}
+
+# path parameters should always go after static paths 
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+            
