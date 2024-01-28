3e5c716f893b6b8a036f2140aa422f3b00c7b329
Chaa Loftin <chaamail@yahoo.com>
2024-01-27 19:17:00 -0500

    added dynamic path generation for path params

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class ValidUserId(str, Enum):
+    Jane = "Jane"
+    John = "John"
 
 @app.get("/users/me")
 async def read_user_me():
@@ -13,6 +13,14 @@ async def read_user_me():
 
 # path parameters should always go after static paths 
 @app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-            
+async def read_user(user_id: ValidUserId):
+    if user_id is ValidUserId.Jane:
+        return {"user_id": user_id}
+    
+    if user_id is ValidUserId.John:
+        return {"user_id": user_id}
+
+# Can be used to generate a static path like /files/home/johndoe/myfile.txt
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
