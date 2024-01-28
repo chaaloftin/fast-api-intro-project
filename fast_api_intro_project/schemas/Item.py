fd9cc4680cec15575e72e3b1ec486388e6bed3c4
Chaa Loftin <chaamail@yahoo.com>
2024-01-28 15:19:53 -0500

    added Pydantic Model Field validation

--- fast_api_intro_project/schemas/Item.py
+++ fast_api_intro_project/schemas/Item.py
@@ -1,10 +1,14 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ItemModel(BaseModel):
     name: str
-    description: Union[str, None] = None
-    price: float
+
+    # Use Fields to 
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: Union[float, None] = None
 
